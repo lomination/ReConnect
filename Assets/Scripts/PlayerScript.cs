4125c255bb32f6bae045192b30be33d1091e19f6
lomination <lomination@rejna.org>
2024-12-26 18:08:41 +0100

    Making the mp core with Mirror 14/22

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -9,12 +9,46 @@ namespace QuickStart
         public GameObject floatingInfo;
 
         private Material _playerMaterialClone;
+        
+        private SceneScript sceneScript;
+        
+        private int selectedWeaponLocal = 1;
+        public GameObject[] weaponArray;
 
         [SyncVar(hook = nameof(OnNameChanged))]
         public string playerName;
 
         [SyncVar(hook = nameof(OnColorChanged))]
         public Color playerColor = Color.white;
+        
+        [SyncVar(hook = nameof(OnWeaponChanged))]
+        public int activeWeaponSynced = 1;
+
+        void Awake()
+        {
+            //allow all players to run this
+            sceneScript = GameObject.FindFirstObjectByType<SceneScript>(); // Changed the deprecated FindObjectOfType
+            // disable all weapons
+            foreach (var item in weaponArray)
+                if (item != null)
+                    item.SetActive(false);
+        }
+
+        [Command]
+        public void CmdSendPlayerMessage()
+        {
+            if (sceneScript) 
+                sceneScript.statusText = $"{playerName} says hello {Random.Range(10, 99)}";
+        }
+
+        [Command]
+        public void CmdSetupPlayer(string _name, Color _col)
+        {
+            //player info sent to server, then server updates sync vars which handles it on all clients
+            playerName = _name;
+            playerColor = _col;
+            sceneScript.statusText = $"{playerName} joined.";
+        }
 
         private void OnNameChanged(string _Old, string _New)
         {
@@ -31,6 +65,8 @@ namespace QuickStart
 
         public override void OnStartLocalPlayer()
         {
+            sceneScript.playerScript = this;
+            
             Camera.main.transform.SetParent(transform);
             Camera.main.transform.localPosition = new Vector3(0, 0, 0);
             
@@ -40,14 +76,6 @@ namespace QuickStart
             string name = "Player" + Random.Range(100, 999);
             Color color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
             CmdSetupPlayer(name, color);
-        }
-
-        [Command]
-        public void CmdSetupPlayer(string _name, Color _col)
-        {
-            // player info sent to server, then server updates sync vars which handles it on all clients
-            playerName = _name;
-            playerColor = _col;
         }
 
         void Update()
@@ -64,6 +92,35 @@ namespace QuickStart
 
             transform.Rotate(0, moveX, 0);
             transform.Translate(0, 0, moveZ);
+            
+            if (Input.GetButtonDown("Fire2")) //Fire2 is mouse 2nd click and left alt
+            {
+                selectedWeaponLocal += 1;
+
+                if (selectedWeaponLocal > weaponArray.Length) 
+                    selectedWeaponLocal = 1; 
+
+                CmdChangeActiveWeapon(selectedWeaponLocal);
+            }
+        }
+        
+        void OnWeaponChanged(int _Old, int _New)
+        {
+            // disable old weapon
+            // in range and not null
+            if (0 < _Old && _Old < weaponArray.Length && weaponArray[_Old] != null)
+                weaponArray[_Old].SetActive(false);
+    
+            // enable new weapon
+            // in range and not null
+            if (0 < _New && _New < weaponArray.Length && weaponArray[_New] != null)
+                weaponArray[_New].SetActive(true);
+        }
+
+        [Command]
+        public void CmdChangeActiveWeapon(int newIndex)
+        {
+            activeWeaponSynced = newIndex;
         }
     }
 }
